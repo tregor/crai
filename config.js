30c554aa9c629f26bd9672390c8643e2ee850cce
tregor <tregor1997@gmail.com>
2023-04-22 22:38:16 -0700

    fix

--- config.js
+++ config.js
@@ -1,26 +1,4 @@
 module.exports = {
     // Глобальные настройки
     defaultSpawn: Game.spawns['Spawn1'],
-    tiers: [
-        {
-            tier: 1,
-            cost: 300,
-        },
-        {
-            tier: 2,
-            cost: 550,
-        },
-        {
-            tier: 3,
-            cost: 800,
-        },
-        {
-            tier: 4,
-            cost: 1300,
-        },
-        {
-            tier: 5,
-            cost: 1800,
-        },
-    ],
 };
