70d06c5166e3beccd6d6c26d308ed39c3030683e
tregor <tregor1997@gmail.com>
2023-04-23 21:31:47 -0700

    Builder - Improved success rate

--- role.builder.js
+++ role.builder.js
@@ -6,17 +6,17 @@ module.exports = {
         default: true,
     },
     /** @param {Creep} creep **/
-    run: function(creep) {
-        if(creep.memory.building && creep.store[RESOURCE_ENERGY] === 0) {
+    run: function (creep) {
+        if (creep.memory.building && creep.store[RESOURCE_ENERGY] === 0) {
             creep.memory.building = false;
             creep.say('🔄 harvest');
         }
-        if(!creep.memory.building && creep.store.getFreeCapacity() === 0) {
+        if (!creep.memory.building && creep.store.getFreeCapacity() === 0) {
             creep.memory.building = true;
             creep.say('🚧 build');
         }
 
-        if(creep.memory.building) {
+        if (creep.memory.building) {
             let targets = [];
             for (let priority of config.constructionSitePriority) {
                 targets = creep.room.find(FIND_CONSTRUCTION_SITES, {
@@ -40,34 +40,31 @@ module.exports = {
             });
             if (targets.length > 0) {
                 targets.sort((a, b) => a.hits / a.hitsMax - b.hits / b.hitsMax);
-                if(creep.repair(targets[0]) === ERR_NOT_IN_RANGE) {
+                if (creep.repair(targets[0]) === ERR_NOT_IN_RANGE) {
                     creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
                 }
             }
-        }
-        else {
+        } else {
             const container = creep.pos.findClosestByPath(FIND_STRUCTURES, {
                 filter: (structure) => {
                     return (structure.structureType === STRUCTURE_CONTAINER && structure.store[RESOURCE_ENERGY] > 0);
                 }
             });
-            if(container) {
-                if(creep.withdraw(container, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
+            if (container) {
+                if (creep.withdraw(container, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
                     creep.moveTo(container, {visualizePathStyle: {stroke: '#ffaa00'}});
                 }
-            }
-            else {
+            } else {
                 const energy = creep.pos.findClosestByPath(FIND_DROPPED_RESOURCES, {
                     filter: (resource) => {
                         return (resource.resourceType === RESOURCE_ENERGY && (resource.amount > creep.store.getFreeCapacity(RESOURCE_ENERGY)));
                     }
                 });
-                if(energy) {
-                    if(creep.pickup(energy) === ERR_NOT_IN_RANGE) {
+                if (energy) {
+                    if (creep.pickup(energy) === ERR_NOT_IN_RANGE) {
                         creep.moveTo(energy, {visualizePathStyle: {stroke: '#ffaa00'}});
                     }
-                }
-                else {
+                } else {
                     const source = creep.pos.findClosestByPath(FIND_SOURCES_ACTIVE);
                     if (source) {
                         if (creep.harvest(source) === ERR_NOT_IN_RANGE) {
@@ -79,35 +76,29 @@ module.exports = {
         }
     },
     getSuccessRate: function (room) {
-        const numBuilders = room.find(FIND_MY_CREEPS, {filter: {memory: {role: 'builder'}}}).length;
-        const numSitesToBuild = room.find(FIND_CONSTRUCTION_SITES).length;
-        const numDamagedStructures = room.find(FIND_STRUCTURES, {
+        const builders = room.find(FIND_MY_CREEPS, {filter: {memory: {role: 'builder'}}}).length;
+        const constructionSites = room.find(FIND_CONSTRUCTION_SITES);
+        const damagedStructures = room.find(FIND_STRUCTURES, {
             filter: (structure) => {
                 return structure.hits < structure.hitsMax;
             }
         }).length;
-        // const constructionSites = room.find(FIND_CONSTRUCTION_SITES);
-        // const sitesToBuild = _.filter(constructionSites, (s) => s.my && !s.progressTotal);
-        // const numSitesToBuild = sitesToBuild.length;
+        const energyAvailable = _.sum(builders, (c) => (c.getActiveBodyparts(WORK) * BUILD_POWER));
+        const energyNeededForConstructs = _.sum(constructionSites, (s) => CONSTRUCTION_COST[s.structureType]);
+        const energyNeededForRepairs = _.sum(damagedStructures, (s) => ((s.hitsMax - s.hits) * REPAIR_COST));
+        const energyNeeded = energyNeededForConstructs + energyNeededForRepairs;
+        const energyRatio = (energyAvailable / energyNeeded) || 0;
 
-        if (numBuilders === 0) {
+        if (builders.length === 0) {
             return 0;
         }
-        if ((numSitesToBuild === 0) && (numDamagedStructures === 0)) {
-            return numBuilders;
+        if ((constructionSites.length === 0) && (damagedStructures.length === 0)) {
+            return builders.length;
         }
 
-        const maxEffort = 100 * numBuilders;
-        let effortSpent = 0;
-        if (numDamagedStructures > 0) {
-            effortSpent += Math.min(maxEffort, numDamagedStructures * REPAIR_POWER * REPAIR_COST);
-        }
-        if (numSitesToBuild > 0) {
-            effortSpent += Math.min(maxEffort - effortSpent, numSitesToBuild * BUILD_POWER * 3000);
-        }
-
-        return (Math.min(effortSpent, 1) / maxEffort);
+        return energyRatio;
     },
+
 
     /** @param {number} tier **/
     getBody: function (tier) {
