a29b712ad41eac6bdf39b3d7d2b02316c088af46
tregor <tregor1997@gmail.com>
2023-05-10 10:46:17 -0700

    Main loop - memory hack

--- main.js
+++ main.js
@@ -9,18 +9,50 @@ const creepRoles = require('./roles');
 require('extends.Creep');
 
 module.exports = {
-    loop: function () {
+    loop:  wrapLoop(function () {
         while (Game.cpu.tickLimit * 0.8 > Game.cpu.getUsed()) {
             if (!this.run()) {
                 break;
             }
         }
-    },
+    }),
     run: function(){
-        let res1 = roomManager.run();
-        let res2 = creepsController.run();
-        let res3 = towerController.run();
-        let res4 = spawnController.run();
-        return (res1 && res2 && res3 && res4)
+        roomManager.run();
+        creepsController.run();
+        towerController.run();
+        spawnController.run();
     }
 };
+
+function wrapLoop(fn) {
+    let memory;
+    let tick;
+
+    return () => {
+        if (tick && tick + 1 === Game.time && memory) {
+            // this line is required to disable the default Memory deserialization
+            delete global.Memory;
+            Memory = memory;
+        } else {
+            memory = Memory;
+        }
+
+        tick = Game.time;
+
+        fn();
+
+        // there are two ways of saving Memory with different advantages and disadvantages
+        // 1. RawMemory.set(JSON.stringify(Memory));
+        // + ability to use custom serialization method
+        // - you have to pay for serialization
+        // - unable to edit Memory via Memory watcher or console
+        // 2. RawMemory._parsed = Memory;
+        // - undocumented functionality, could get removed at any time
+        // + the server will take care of serialization, it doesn't cost any CPU on your site
+        // + maintain full functionality including Memory watcher and console
+
+        // this implementation uses the official way of saving Memory
+//        RawMemory.set(JSON.stringify(Memory));
+        RawMemory._parsed = Memory;
+    };
+}
