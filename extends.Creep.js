e747364b8a081a3f0a12c4ccc72604033f2f752a
tregor <tregor1997@gmail.com>
2023-05-10 14:25:03 -0700

    utils added

--- extends.Creep.js
+++ extends.Creep.js
@@ -72,6 +72,36 @@ if (typeof Creep.prototype.idleFor !== 'function'){
         }
     };
 }
+// proximo 30 October 2016 at 04:57
+
+/**
+ * Creep method optimizations "getActiveBodyparts"
+ */
+Creep.prototype.getActiveBodyparts = function (type) {
+    var count = 0;
+    for (var i = this.body.length; i-- > 0;) {
+        if (this.body[i].hits > 0) {
+            if (this.body[i].type === type) {
+                count++;
+            }
+        } else break;
+    }
+    return count;
+};
+
+/**
+ * Fast check if bodypart exists
+ */
+Creep.prototype.hasActiveBodyparts = function (type) {
+    for (var i = this.body.length; i-- > 0;) {
+        if (this.body[i].hits > 0) {
+            if (this.body[i].type === type) {
+                return true;
+            }
+        } else break;
+    }
+    return false;
+};
 
 Creep.prototype.getFullname = function () {
     if (!this.memory.tier || !this.memory.role) {
